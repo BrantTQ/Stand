17fd87122473aa7d024609bcc01c40344ce64663
Vincent Aboagye <aboagye.vynce@gmail.com>
2025-09-16 17:13:54 +0200

    transition and ai video

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sim.dataexplorer',
+  appName: 'sim-life-stand',
+  webDir: 'dist'
+};
+
+export default config;
